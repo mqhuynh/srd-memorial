2ac20ec696f28dbb0034d56298fbeb0b5ebc7c78
Anish Gyawali <anish.gyawali@selu.edu>
2021-09-29 18:28:03 -0500

    Added logout function to Homepage

--- srd-memorial/src/app/home/home.page.ts
+++ srd-memorial/src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+@Injectable({
+  providedIn: 'root'
+})
+export class HomePage implements OnInit {
+user:any;
+  constructor(private auth: AuthService) {}
+  ngOnInit() {
+    this.auth.user$.subscribe(user=>{
+      this.user=user;
+      console.log(user);
+    })
+  }
 
-  constructor() {}
+  logout() {
+    this.auth.signOut();
+  }//end of logout
 
 }
--- srd-memorial/src/app/models/user.ts
+++ srd-memorial/src/app/models/user.ts
@@ -1,6 +1,7 @@
 export interface User {
     userId:string;
     userEmail:string;
+    userPhoto:string;
     userFirstname:string;
     userLastname:string;
 }
