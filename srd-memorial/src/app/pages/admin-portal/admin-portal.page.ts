0d0513999565e394e0d3aa6a33fe86a001c52d5a
Your Name <69984667+RoshanBhandary@users.noreply.github.com>
2021-11-15 15:52:28 -0600

    updated html & ts

--- srd-memorial/src/app/pages/admin-portal/admin-portal.page.ts
+++ srd-memorial/src/app/pages/admin-portal/admin-portal.page.ts
@@ -12,4 +12,40 @@ export class AdminPortalPage implements OnInit {
   constructor(private auth: AuthService) { }
 
   ngOnInit() {
-    
+    this.auth.getAllUsers().subscribe((data) => {
+      this.user= data.map((e) => ({
+          id: e.payload.doc.id,
+          isedit: false,
+           email: e.payload.doc.data()['userEmail'],
+          firstname: e.payload.doc.data()['userFirstname'],
+          lastname: e.payload.doc.data()['userLastname'],
+          isApproved: e.payload.doc.data()['IsApproved'],
+
+        }));
+      console.log(this.user);
+    });
+  }
+  updateUserInfo(id: string) {
+    console.log(id);
+  }
+
+  editUser(User){
+    User.isedit = true ;
+    User.editPermission = User.isApproved;
+
+  }
+
+  updateUser(userdata){
+    let data = {};
+    data ['IsApproved'] = userdata.isApproved;
+    this.auth.updateUser(userdata.id, data);
+    userdata.isedit = false;
+
+  }
+
+  deleteUser(user_id){
+    this.auth.deleteUser(user_id);
+  }
+
+}
+
